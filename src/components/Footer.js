7f90fe11f23c8b9229113c01b51eeeddee35ef9a
Luke Franco <94336719+LukeFranco@users.noreply.github.com>
2023-09-23 14:51:03 -0400

    Changed back to FooterStyles.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,71 @@
 import React from "react";
-import styles from "../styles/footer.css";
-
+import {
+  Box,
+  Container,
+  Row,
+  Column,
+  FooterLink,
+  Heading,
+} from "./FooterStyles";
+  
 const Footer = () => {
   return (
-    <Box className="Box">
-      <h1>GeeksforGeeks: A Computer Science Portal for Geeks</h1>
-      <Container className="Container">
-        <Row className="Row">
-          <Column className="Column">
-            <Heading className="Heading">About Us</Heading>
-            <FooterLink className="FooterLink" href="#">Aim</FooterLink>
-            <FooterLink className="FooterLink" href="#">Vision</FooterLink>
-            <FooterLink className="FooterLink" href="#">Testimonials</FooterLink>
+    <Box>
+      <h1 style={{ color: "green", 
+                   textAlign: "center", 
+                   marginTop: "-50px" }}>
+        GeeksforGeeks: A Computer Science Portal for Geeks
+      </h1>
+      <Container>
+        <Row>
+          <Column>
+            <Heading>About Us</Heading>
+            <FooterLink href="#">Aim</FooterLink>
+            <FooterLink href="#">Vision</FooterLink>
+            <FooterLink href="#">Testimonials</FooterLink>
           </Column>
-          <Column className="Column">
-            <Heading className="Heading">Services</Heading>
-            <FooterLink className="FooterLink" href="#">Writing</FooterLink>
-            <FooterLink className="FooterLink" href="#">Internships</FooterLink>
-            <FooterLink className="FooterLink" href="#">Coding</FooterLink>
-            <FooterLink className="FooterLink" href="#">Teaching</FooterLink>
+          <Column>
+            <Heading>Services</Heading>
+            <FooterLink href="#">Writing</FooterLink>
+            <FooterLink href="#">Internships</FooterLink>
+            <FooterLink href="#">Coding</FooterLink>
+            <FooterLink href="#">Teaching</FooterLink>
           </Column>
-          <Column className="Column">
-            <Heading className="Heading">Contact Us</Heading>
-            <FooterLink className="FooterLink" href="#">Uttar Pradesh</FooterLink>
-            <FooterLink className="FooterLink" href="#">Ahemdabad</FooterLink>
-            <FooterLink className="FooterLink" href="#">Indore</FooterLink>
-            <FooterLink className="FooterLink" href="#">Mumbai</FooterLink>
+          <Column>
+            <Heading>Contact Us</Heading>
+            <FooterLink href="#">Uttar Pradesh</FooterLink>
+            <FooterLink href="#">Ahemdabad</FooterLink>
+            <FooterLink href="#">Indore</FooterLink>
+            <FooterLink href="#">Mumbai</FooterLink>
           </Column>
-          <Column className="Column">
-            <Heading className="Heading">Social Media</Heading>
-            <FooterLink className="FooterLink" href="#">
+          <Column>
+            <Heading>Social Media</Heading>
+            <FooterLink href="#">
               <i className="fab fa-facebook-f">
-                <span>Facebook</span>
+                <span style={{ marginLeft: "10px" }}>
+                  Facebook
+                </span>
               </i>
             </FooterLink>
-            <FooterLink className="FooterLink" href="#">
+            <FooterLink href="#">
               <i className="fab fa-instagram">
-                <span>Instagram</span>
+                <span style={{ marginLeft: "10px" }}>
+                  Instagram
+                </span>
               </i>
             </FooterLink>
-            <FooterLink className="FooterLink" href="#">
+            <FooterLink href="#">
               <i className="fab fa-twitter">
-                <span>Twitter</span>
+                <span style={{ marginLeft: "10px" }}>
+                  Twitter
+                </span>
               </i>
             </FooterLink>
-            <FooterLink className="FooterLink" href="#">
+            <FooterLink href="#">
               <i className="fab fa-youtube">
-                <span>Youtube</span>
+                <span style={{ marginLeft: "10px" }}>
+                  Youtube
+                </span>
               </i>
             </FooterLink>
           </Column>
